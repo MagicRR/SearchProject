5894ef00472cb39eaf4476c899fcda57af8b008d
MagicRR <r.richard.94@orange.fr>
2018-03-13 09:57:48 +0100

    push index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Recherche</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+    </head>
+
+    <body>
+
+            <div class="form-group row">
+                <form method="post" action="" style="width: 100%;">
+                    <input type="text" class="col-lg-4 col-md-4 col-sm-4 col-xs-12 form-control" style="display: inline;" placeholder="Search..." required/><br/><br/>
+
+                    <button type="submit" class="btn btn-primary">Search</button>
+                </form>
+            </div>
+
+    </body>
+
+</html>
